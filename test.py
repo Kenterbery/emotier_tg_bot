752146796f180cd0f380f509e3b10f04b00e3e48
Kenterbery <your.earshot@gmail.com>
2021-01-19 18:48:27 +0200

    Add download_file func and add AudioWorker to main

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-import telegram
+import logging
+
 from decouple import config
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import logging
-import librosa
-import subprocess
+
+from utils import AudioWorker
 
 TOKEN = config("TOKEN")
 
@@ -12,6 +12,11 @@ dispatcher = updater.dispatcher
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
+
+def download_file(file):
+    file_path = file.file_path.rsplit("/", 1)[-1]
+    file.download()
+    return file_path
 
 
 def start(update, context):
@@ -25,16 +30,8 @@ def echo(update, context):
 
 def voice_reply(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'd receive your voice, thanks! Start processing...")
-
-    file = context.bot.get_file(update.message.voice.file_id)
-    file_path = file.file_path.rsplit("/", 1)[-1]
-    file.download()
-
-    process = subprocess.run(["ftransc", "-f", "ogg", file_path])
-    file_path = file_path.partition(".oga")[0] + ".ogg"
-
-    data, sr = librosa.load(file_path)
-    print(len(data))
+    file_path = download_file(context.bot.get_file(update.message.voice.file_id))
+    feature_vector = AudioWorker().fit(file_path)
 
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'd receive your voice, thanks!")
 
