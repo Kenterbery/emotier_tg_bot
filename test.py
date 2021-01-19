268a2b79dbb5d29711714506c0d42816a57a97d3
Kenterbery <your.earshot@gmail.com>
2021-01-19 13:27:03 +0200

    Simple handler of messages. Handle a text and voice message to reply

---
+++ test.py
@@ -0,0 +1,36 @@
+import telegram
+from decouple import config
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+TOKEN = config("TOKEN")
+
+updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please, talk to me!")
+
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+def voice_reply(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'd receive your voice, thanks!")
+
+start_handler = CommandHandler("start", start)
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+voice_handler = MessageHandler(Filters.voice, voice_reply)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(echo_handler)
+dispatcher.add_handler(voice_handler)
+
+
+if __name__ == '__main__':
+    updater.start_polling()
